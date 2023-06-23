d5159806b4686b5a08d3894e9940fc56604e3a30
daeung <hyperreal@naver.com>
2023-06-23 10:50:31 +0900

    add Lora Link, Sam

---
+++ app.py
@@ -0,0 +1,125 @@
+import os
+HOME = os.getcwd()
+print("HOME:", HOME)
+
+CHECKPOINT_PATH = os.path.join(HOME, "weights", "sam_vit_h_4b8939.pth")
+print(CHECKPOINT_PATH, "; exist:", os.path.isfile(CHECKPOINT_PATH))
+
+import torch
+
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+MODEL_TYPE = "vit_h"
+
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+
+sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH).to(device=DEVICE)
+
+mask_generator = SamAutomaticMaskGenerator(sam)
+
+import os
+
+IMAGE_NAME = "dog.jpeg"
+IMAGE_PATH = os.path.join(HOME, "data", IMAGE_NAME)
+
+import cv2
+import supervision as sv
+
+image_bgr = cv2.imread(IMAGE_PATH)
+image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+sam_result = mask_generator.generate(image_rgb)
+
+
+
+mask_annotator = sv.MaskAnnotator()
+
+detections = sv.Detections.from_sam(sam_result=sam_result)
+
+annotated_image = mask_annotator.annotate(scene=image_bgr.copy(), detections=detections)
+
+sv.plot_images_grid(
+    images=[image_bgr, annotated_image],
+    grid_size=(1, 2),
+    titles=['source image', 'segmented image']
+)
+
+masks = [
+    mask['segmentation']
+    for mask
+    in sorted(sam_result, key=lambda x: x['area'], reverse=True)
+]
+
+sv.plot_images_grid(
+    images=masks,
+    grid_size=(8, int(len(masks) / 8)),
+    size=(16, 16)
+)
+
+mask_predictor = SamPredictor(sam)
+
+IMAGE_NAME = "dog.jpeg"
+IMAGE_PATH = os.path.join(HOME, "data", IMAGE_NAME)
+
+import base64
+
+def encode_image(filepath):
+    with open(filepath, 'rb') as f:
+        image_bytes = f.read()
+    encoded = str(base64.b64encode(image_bytes), 'utf-8')
+    return "data:image/jpg;base64,"+encoded
+
+from jupyter_bbox_widget import BBoxWidget
+
+widget = BBoxWidget()
+widget.image = encode_image(IMAGE_PATH)
+
+import numpy as np
+
+# default_box is going to be used if you will not draw any box on image above
+default_box = {'x': 68, 'y': 247, 'width': 555, 'height': 678, 'label': ''}
+
+box = widget.bboxes[0] if widget.bboxes else default_box
+box = np.array([
+    box['x'], 
+    box['y'], 
+    box['x'] + box['width'], 
+    box['y'] + box['height']
+])
+
+import supervision as sv
+
+image_bgr = cv2.imread(IMAGE_PATH)
+image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+
+mask_predictor.set_image(image_rgb)
+
+masks, scores, logits = mask_predictor.predict(
+    box=box,
+    multimask_output=True
+)
+
+box_annotator = sv.BoxAnnotator(color=sv.Color.red())
+mask_annotator = sv.MaskAnnotator(color=sv.Color.red())
+
+detections = sv.Detections(
+    xyxy=sv.mask_to_xyxy(masks=masks),
+    mask=masks
+)
+detections = detections[detections.area == np.max(detections.area)]
+
+source_image = box_annotator.annotate(scene=image_bgr.copy(), detections=detections, skip_label=True)
+segmented_image = mask_annotator.annotate(scene=image_bgr.copy(), detections=detections)
+
+sv.plot_images_grid(
+    images=[source_image, segmented_image],
+    grid_size=(1, 2),
+    titles=['source image', 'segmented image']
+)
+
+import supervision as v
+
+sv.plot_images_grid(
+    images=masks,
+    grid_size=(1, 4),
+    size=(16, 4)
+)
